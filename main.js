9caef16f0b16a0bf3aaec5e5a176aa8fcce46344
Aditya_Vispute <151549687+adityavispute005@users.noreply.github.com>
2023-11-24 18:34:49 +0530

    all files are updated
    added delete button and some UI changes

--- main.js
+++ main.js
@@ -7,6 +7,13 @@ function submitAppointment() {
     if (name === "" || email === "" || phone === "") {
         alert("Please fill in all fields.");
         return;
+        var appointment = { name: name, email: email, phone: phone };
+        var appointments = JSON.parse(localStorage.getItem('appointments')) || [];
+        appointments.push(appointment);
+        localStorage.setItem('appointments', JSON.stringify(appointments));
+    
+        // Display data on the screen
+        displayAppointments();
     }
 
     // Save data to local storage
@@ -27,11 +34,30 @@ function displayAppointments() {
     appointmentsList.innerHTML = "";
 
     // Display each appointment
-    appointments.forEach(function (appointment) {
+    appointments.forEach(function (appointment, index) {
         var listItem = document.createElement('li');
-        listItem.textContent = `Name: ${appointment.name}, Email: ${appointment.email}, Phone: ${appointment.phone}`;
+        listItem.innerHTML = `
+            <span>Name: ${appointment.name}</span>
+            <span>Email: ${appointment.email}</span>
+            <span>Phone: ${appointment.phone}</span>
+            <button class="delete" onclick="deleteAppointment(${index})">Delete</button>
+        `;
+
         appointmentsList.appendChild(listItem);
     });
+}
+
+function deleteAppointment(index) {
+    var appointments = JSON.parse(localStorage.getItem('appointments')) || [];
+
+    // Remove the appointment from the array
+    appointments.splice(index, 1);
+
+    // Update local storage
+    localStorage.setItem('appointments', JSON.stringify(appointments));
+
+    // Update the displayed appointments
+    displayAppointments();
 }
 
 // Display existing appointments on page load
