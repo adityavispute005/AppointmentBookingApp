4f3369b8e63b04c49623bf4f6e8ea161527a2b3a
Aditya_Vispute <151549687+adityavispute005@users.noreply.github.com>
2023-11-24 22:28:03 +0530

    Edit button added

--- main.js
+++ main.js
@@ -7,23 +7,28 @@ function submitAppointment() {
     if (name === "" || email === "" || phone === "") {
         alert("Please fill in all fields.");
         return;
+    }
+
+    // Check if editing an existing appointment
+    var editIndex = document.getElementById('editIndex').value;
+
+    if (editIndex !== "") {
+        // Editing an existing appointment
+        editAppointment(editIndex, name, email, phone);
+    } else {
+        // Save data to local storage
         var appointment = { name: name, email: email, phone: phone };
         var appointments = JSON.parse(localStorage.getItem('appointments')) || [];
         appointments.push(appointment);
         localStorage.setItem('appointments', JSON.stringify(appointments));
-    
+
         // Display data on the screen
         displayAppointments();
     }
 
-    // Save data to local storage
-    var appointment = { name: name, email: email, phone: phone };
-    var appointments = JSON.parse(localStorage.getItem('appointments')) || [];
-    appointments.push(appointment);
-    localStorage.setItem('appointments', JSON.stringify(appointments));
-
-    // Display data on the screen
-    displayAppointments();
+    // Reset the form
+    document.getElementById('appointmentForm').reset();
+    document.getElementById('editIndex').value = "";
 }
 
 function displayAppointments() {
@@ -40,6 +45,7 @@ function displayAppointments() {
             <span>Name: ${appointment.name}</span>
             <span>Email: ${appointment.email}</span>
             <span>Phone: ${appointment.phone}</span>
+            <button class="edit" onclick="prepareEdit(${index})">Edit</button>
             <button class="delete" onclick="deleteAppointment(${index})">Delete</button>
         `;
 
@@ -47,11 +53,23 @@ function displayAppointments() {
     });
 }
 
-function deleteAppointment(index) {
+function prepareEdit(index) {
     var appointments = JSON.parse(localStorage.getItem('appointments')) || [];
+    var appointment = appointments[index];
 
-    // Remove the appointment from the array
-    appointments.splice(index, 1);
+    // Set values in the form for editing
+    document.getElementById('name').value = appointment.name;
+    document.getElementById('email').value = appointment.email;
+    document.getElementById('phone').value = appointment.phone;
+    document.getElementById('editIndex').value = index;
+}
+
+function editAppointment(index, newName, newEmail, newPhone) {
+    var appointments = JSON.parse(localStorage.getItem('appointments')) || [];
+    var editedAppointment = { name: newName, email: newEmail, phone: newPhone };
+
+    // Update the appointment in the array
+    appointments[index] = editedAppointment;
 
     // Update local storage
     localStorage.setItem('appointments', JSON.stringify(appointments));
@@ -59,6 +77,3 @@ function deleteAppointment(index) {
     // Update the displayed appointments
     displayAppointments();
 }
-
-// Display existing appointments on page load
-displayAppointments();
