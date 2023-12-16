270c2608ea1e8e6a1aae6acd91a015d2571634a7
Aditya_Vispute <151549687+adityavispute005@users.noreply.github.com>
2023-12-16 22:57:28 +0530

    edit button added

--- main.js
+++ main.js
@@ -13,41 +13,51 @@ function updateUserDetails(userDetailsArray) {
 function initUI() {
     // Fetch user details from the cloud
     fetchUserDetails()
-        .then(function (response) {
-            var userDetailsArray = response.data;
+    .then(function (response) {
+        var userDetailsArray = response.data;
 
-            // Display user details in the UI
-            var userList = document.getElementById('userList');
-            userList.innerHTML = ''; // Clear previous entries
+        // Display user details in the UI
+        var userList = document.getElementById('userList');
+        userList.innerHTML = ''; // Clear previous entries
 
-            userDetailsArray.forEach(function (userDetails, index) {
-                var listItem = document.createElement('li');
+        userDetailsArray.forEach(function (userDetails, index) {
+            var listItem = document.createElement('li');
 
-                // Unicode character for the trash can icon
-                var trashIcon = '\u{1F5D1}'; // You can customize this to your liking
+            // Unicode characters for the edit and delete icons
+            var editIcon = '\u{1F58A}'; // Edit icon
+            var deleteIcon = '\u{1F5D1}'; // Delete icon
 
-                // Create a delete icon for each user
-                var deleteButton = document.createElement('button');
-                deleteButton.innerHTML = trashIcon;
-                deleteButton.style.cursor = 'pointer';
-                deleteButton.onclick = function () {
-                    deleteUser(index, userDetails._id);
-                };
+            // Create an edit button for each user
+            var editButton = document.createElement('button');
+            editButton.innerHTML = editIcon;
+            editButton.style.cursor = 'pointer';
+            editButton.onclick = function () {
+                editUser(index, userDetails);
+            };
 
-                // Display user details in the list item
-                listItem.textContent = userDetails.firstName + ' ' + userDetails.lastName + ' ' + userDetails.email + ' ' + userDetails.address + ' ' + userDetails.Phone_no + ' ';
+            // Create a delete button for each user
+            var deleteButton = document.createElement('button');
+            deleteButton.innerHTML = deleteIcon;
+            deleteButton.style.cursor = 'pointer';
+            deleteButton.onclick = function () {
+                deleteUser(index, userDetails._id);
+            };
 
-                // Append the delete button to the list item
-                listItem.appendChild(deleteButton);
+            // Display user details in the list item
+            listItem.textContent = userDetails.firstName + ' ' + userDetails.lastName + ' ' + userDetails.email + ' ' + userDetails.address + ' ' + userDetails.Phone_no + ' ';
 
-                // Append the list item to the user list
-                userList.appendChild(listItem);
-            });
-        })
-        .catch(function (error) {
-            // Handle errors if the GET request fails
-            console.error('Error fetching data from the cloud:', error);
+            // Append the edit and delete buttons to the list item
+            listItem.appendChild(editButton);
+            listItem.appendChild(deleteButton);
+
+            // Append the list item to the user list
+            userList.appendChild(listItem);
         });
+    })
+    .catch(function (error) {
+        // Handle errors if the GET request fails
+        console.error('Error fetching data from the cloud:', error);
+    });
 }
 
 // Function to delete a user by index
@@ -81,6 +91,50 @@ function deleteUser(index, userId) {
             console.error('Error fetching data from the cloud:', error);
         });
 }
+
+// Function to handle user editing
+function editUser(index, userDetails) {
+    // Populate the form with user details for editing
+    document.getElementById('firstName').value = userDetails.firstName;
+    document.getElementById('lastName').value = userDetails.lastName;
+    document.getElementById('email').value = userDetails.email;
+    document.getElementById('address').value = userDetails.address;
+    document.getElementById('Phone_no').value = userDetails.Phone_no;
+
+    // Handle form submission for editing
+    document.getElementById('userForm').onsubmit = function (event) {
+        event.preventDefault();
+        // Update the user details array with edited details
+        userDetailsArray[index] = {
+            firstName: document.getElementById('firstName').value,
+            lastName: document.getElementById('lastName').value,
+            email: document.getElementById('email').value,
+            address: document.getElementById('address').value,
+            Phone_no: document.getElementById('Phone_no').value,
+            _id: userDetails._id // Keep the original ID
+        };
+
+        // Update user details in the cloud
+        updateUserDetails(userDetailsArray)
+            .then(function (putResponse) {
+                // Handle the success response if needed
+                console.log('Data updated in the cloud:', putResponse.data);
+
+                // Reinitialize the UI
+                initUI();
+            })
+            .catch(function (putError) {
+                // Handle errors if the PUT request fails
+                console.error('Error updating data in the cloud:', putError);
+            });
+
+        // Clear the form after submission
+        document.getElementById('userForm').reset();
+        // Remove the event listener to avoid unintentional form submissions
+        document.getElementById('userForm').onsubmit = submitForm;
+    };
+}
+
 
 // Function to handle form submission
 function submitForm() {
